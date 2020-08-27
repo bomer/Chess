671e9ecbc53af57667dc2e0e0d72b739e3e9ca46
James O'Toole <james@nomoss.co>
2020-08-28 00:57:10 +1000

    Just getting started

---
+++ README.md
@@ -0,0 +1,18 @@
+# Chess in C++
+
+I never finished created Chess in c++ so I thought I would do so.
+
+## Cool
+
+Yup
+
+## TODO 
+
+[] Create board, print board
+[] Game Loop
+[] Pieces
+[] Movments and stubs for checks
+[] Piece Checks
+[] Win Checks
+[] Check + Checkmate + Stalemate?
+[]  
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+//Using enum for black + White set of possible pieces. Let's see if this is a good idea or not
+// B+W | King, Queen, Bishop, Knight, Rook, Pawn
+enum Piece { bk, bq, bb, bkn, br, bp, wk, wq, wb, wkn, wr, wp};
+
+int main(){
+
+	
+	cout << "hello world \n";
+	return 0;
+}
+
